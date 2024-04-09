3b51dc5e185ae1139efb7730c2812530e90871d3
ayukrisn <ayuukrisn@gmail.com>
2024-04-09 15:08:14 +0800

    same with previous

--- lib/signup_page2.dart
+++ lib/signup_page2.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tugas_progmob_1/login_page.dart';
-import 'package:tugas_progmob_1/signup_page2.dart';
+import 'package:tugas_progmob_1/home_page.dart';
 
-class SignupPage extends StatefulWidget {
-  const SignupPage({Key? key}) : super(key: key);
+class SignupPage2 extends StatefulWidget {
+  const SignupPage2({Key? key}) : super(key: key);
 
   @override
-  State<SignupPage> createState() => _SignupPageState();
+  State<SignupPage2> createState() => _SignupPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
+class _SignupPageState extends State<SignupPage2> {
   final _formKey = GlobalKey<FormState>();
 
   bool _passwordVisible = true;
-  String? _email ='';
-  String? _password ='';
-  String? _confirmPassword = '';
+  String? _name = '';
+  String? _username = '';
+  String? _occupancy = '';
+  DateTime _birthDate = DateTime(1);
+
+  TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate() async {
+    DateTime? _picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1950),
+        lastDate: DateTime.now());
+    setState(() {
+      _dateController.text = _picked.toString().split("")[1];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +50,7 @@ class _SignupPageState extends State<SignupPage> {
               ),
             ),
             const SizedBox(height: 48),
-            Text("Glad to see you!",
+            Text("Great Start!",
                 style: GoogleFonts.baloo2(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
@@ -46,11 +60,11 @@ class _SignupPageState extends State<SignupPage> {
               TextSpan(
                 children: <TextSpan>[
                   TextSpan(
-                    text: 'Start with entering your ',
+                    text: 'One step left! ',
                     style: TextStyle(fontSize: 14, color: Color(0xFF446F84)),
                   ),
                   TextSpan(
-                    text: 'e-mail and password',
+                    text: 'Fill up your biodata below',
                     style: TextStyle(
                       fontSize: 14,
                       color: Color(0xFF446F84),
@@ -58,8 +72,7 @@ class _SignupPageState extends State<SignupPage> {
                     ),
                   ),
                   TextSpan(
-                    text:
-                        ' below, and we will be ready to embark to our new journey!',
+                    text: ' to continue shortly.',
                     style: TextStyle(fontSize: 14, color: Color(0xFF446F84)),
                   ),
                 ],
@@ -74,80 +87,45 @@ class _SignupPageState extends State<SignupPage> {
                   TextFormField(
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Please enter your email.';
-                      }
-                      if (!RegExp(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b',
-                              caseSensitive: false)
-                          .hasMatch(value)) {
-                        return 'Please enter a valid email address.';
+                        return 'Please enter your name.';
                       }
                       return null;
                     },
-                    onSaved: (value) => _email = value,
+                    onSaved: (value) => _name = value,
                     decoration: const InputDecoration(
-                      labelText: 'E-mail',
-                      hintText: 'Enter your e-mail',
+                      labelText: 'Name',
+                      hintText: 'Enter your name',
                     ),
                   ),
                   const SizedBox(height: 16),
                   TextFormField(
-                    obscureText: !_passwordVisible,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Please enter your password.';
+                        return 'Please enter your username.';
                       }
-                      // print(value);
+                      if (value.trim().contains(RegExp(r'\s'))) {
+                        return 'Whitespace is not allowed';
+                      }
                       return null;
                     },
-                    onSaved: (value) => _password = value,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter your password',
-                      suffixIcon: IconButton(
-                          icon: Icon(
-                            _passwordVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: Color(0xFF446F84),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _passwordVisible = !_passwordVisible;
-                            });
-                          }),
+                    onSaved: (value) => _username = value,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Enter your username',
                     ),
                   ),
                   const SizedBox(height: 16),
                   TextFormField(
-                    obscureText: !_passwordVisible,
-                    onSaved: (value) => _confirmPassword = value,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please confirm your password.';
-                      }
-                      if (_confirmPassword != _password) {
-                        print(_password);
-                        print(_confirmPassword);
-                        return "Password doesn't match";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Confirm Password',
-                      hintText: 'Confirm your password',
-                      suffixIcon: IconButton(
-                          icon: Icon(
-                            _passwordVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: Color(0xFF446F84),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _passwordVisible = !_passwordVisible;
-                            });
-                          }),
+                    controller: _dateController,
+                    decoration: const InputDecoration(
+                      labelText: 'Birth Date',
+                      hintText: 'Enter your birth date',
+                      prefixIcon: Icon(Icons.calendar_today),
                     ),
+                    readOnly: true,
+                    onTap: () {
+                      _selectDate();
+                    },
                   ),
                   const SizedBox(height: 120),
                   Row(
@@ -160,7 +138,7 @@ class _SignupPageState extends State<SignupPage> {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => SignupPage2()),
+                                    builder: (context) => HomePage()),
                               );
                             }
                           },
